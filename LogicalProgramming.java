e7590a6ebbf8b4fdae941e5b4db45da4909805e4
Harish mahur <hmahur20@gmail.com>
2022-07-18 23:43:46 +0530

    modified: stopwatch stimulator

--- LogicalProgramming.java
+++ LogicalProgramming.java
@@ -1,36 +1,46 @@
 package com.logical.programming;
 
+import java.util.Scanner;
+
 public class LogicalProgramming {
 
 	public static void main(String[] args) {
-		
-		computeCouponNo();
+		LogicalProgramming sw = new LogicalProgramming();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Press '1' to Start Time: ");
+		sc.nextInt();
+		sw.start();
+
+		System.out.println();
+		System.out.println("Press '2' to Stop Time: ");
+		sc.nextInt();
+		sw.stop();
+
+		long l = sw.getElapsedTime();
+		System.out.println();
+		System.out.println("Total Time Elapsed(in millisec) is:" + l);
+		System.out.println();
+		System.out.println("Converting millisec to seconds: " + (l / 1000) + " sec");
 	}
 
-	static public void computeCouponNo() {
-		int min = 1;
-		int max = 100;
-		int distinctno = 5;
-		int number = 0;
-		int[] coupon = new int[distinctno];
-		boolean isRepeated;
-		for (int i = 0; i < distinctno; i++) {
-			do {
-				isRepeated = false;
-				number = (int) (Math.random() * 10000) % 100;
-				for (int j = 0; j < distinctno; j++) {
-					if (coupon[j] == number) {
-						isRepeated = true;
-						break;
-					}
-				}
-			} while (isRepeated);
-			coupon[i] = number;
-		}
-		System.out.println("The Coupon Number is : ");
-		for (int i = 0; i < distinctno; i++) {
-			System.out.print(coupon[i] + " ");
-		}
+	static long startTime;
+	static long stopTime;
+	static long elapsedTime;
+
+	// to start timer
+	public void start() {
+		startTime = System.currentTimeMillis();
+		System.out.println("Start Time is: " + startTime);
 	}
 
+	// to stop timer
+	public void stop() {
+		stopTime = System.currentTimeMillis();
+		System.out.println("Stop Time is: " + stopTime);
+	}
+
+	public long getElapsedTime() {
+		elapsedTime = stopTime - startTime;
+		return elapsedTime;
+	}
 }
