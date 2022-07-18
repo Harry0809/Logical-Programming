b2714e51204a3601f634030affd995bbe2283e30
Harish mahur <hmahur20@gmail.com>
2022-07-18 23:38:58 +0530

    modified: coupon number

--- LogicalProgramming.java
+++ LogicalProgramming.java
@@ -1,16 +1,36 @@
 package com.logical.programming;
 
-
 public class LogicalProgramming {
 
 	public static void main(String[] args) {
-		int number = 3735536, reverse = 0;
-
-		while (number != 0) {
-			int remainder = number % 10;
-			reverse = reverse * 10 + remainder;
-			number = number / 10;
-		}
-		System.out.println("The reverse of the given number is: " + reverse);
+		
+		computeCouponNo();
 	}
+
+	static public void computeCouponNo() {
+		int min = 1;
+		int max = 100;
+		int distinctno = 5;
+		int number = 0;
+		int[] coupon = new int[distinctno];
+		boolean isRepeated;
+		for (int i = 0; i < distinctno; i++) {
+			do {
+				isRepeated = false;
+				number = (int) (Math.random() * 10000) % 100;
+				for (int j = 0; j < distinctno; j++) {
+					if (coupon[j] == number) {
+						isRepeated = true;
+						break;
+					}
+				}
+			} while (isRepeated);
+			coupon[i] = number;
+		}
+		System.out.println("The Coupon Number is : ");
+		for (int i = 0; i < distinctno; i++) {
+			System.out.print(coupon[i] + " ");
+		}
+	}
+
 }
